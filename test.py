5275b75cc35e6aabb301b9b9ad90be1e35836d59
User <art.bojar4uck97@gmail.com>
2022-11-12 21:54:46 +0300

    added if

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-print("NewName")
-x = 5
-print(x*x)
+#print("NewName")
+#x = 5
+#print(x*x)
+#print("rezult----", 600*x )
+
+# to_int = '10'
+# print(int(to_int))
+
+num1 = int(input ("Input number:"))
+num2 = int(input ("Input number:"))
+
+if num1>num2: print("NUmber1 more than Number2\n","Number1:", "num1\n,"Number2:", num2)
+else: print("NUmber2 more than Number1", num1, num2)
